b5d05e121680d3405780bcea6932eaa6b511d6ea
soumojitd22 <40084876+soumojitd22@users.noreply.github.com>
2018-11-25 15:14:01 +0530

    Minor UI functionality correction

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,20 +9,33 @@ import { NgForm } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
   serviceMessage: any;
+  isWaitingForRespone: boolean = false;
+  submitBtn: String = "Submit";
   student: any = {};
+  studentForm: any = {};
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
 
-  submit(student: NgForm) {
-    this.dataService.save(student).subscribe(
+  submit(studentForm: NgForm) {
+    this.isWaitingForRespone = true;
+    this.submitBtn = "Please wait..";
+    this.dataService.save(studentForm.value).subscribe(
       res => {
         console.log(res);
         this.serviceMessage = res.message;
+        this.isWaitingForRespone = false;
+        this.submitBtn = "Submit";
+        studentForm.resetForm();
       },
-      error => console.error(error)
+      error => {
+        console.error(error);
+        this.serviceMessage = "Ahh!! Something went wrong..";
+        this.isWaitingForRespone = false;
+        this.submitBtn = "Submit";
+      }
     );
   }
 }
